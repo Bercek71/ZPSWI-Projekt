908356424af355c337d478ad7ba743b2d461f0d7
Marek Beran <bercek71@gmail.com>
2024-10-09 20:43:26 +0200

    test fix

--- src/test/java/com/example/ExampleResourceTest.java
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -1,20 +1,29 @@
 package com.example;
 
+import com.persistence.Room;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class ExampleResourceTest {
     @Test
-    void testRoomEndPoint() {
+    void testValidRequest() {
         given()
           .when().get("/rooms")
           .then()
              .statusCode(200);
+    }
+
+    @Test
+    void testInvalidRequest() {
+        given()
+          .when().get("/rooms/afjsgkj")
+          .then()
+             .statusCode(404);
     }
 
 }
