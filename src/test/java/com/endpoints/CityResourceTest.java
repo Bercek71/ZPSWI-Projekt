308a5cfa024cd928556236b44645a8b095c82072
Andromius <miniswiczmartin@gmail.com>
2024-10-27 00:31:43 +0200

    SCRUM-18 Added basic tests for various resources

---
+++ src/test/java/com/endpoints/CityResourceTest.java
@@ -0,0 +1,50 @@
+package com.endpoints;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.*;
+
+@QuarkusTest
+@TestHTTPEndpoint(CityResource.class)
+public class CityResourceTest {
+
+	@Test
+	void findAllShouldAlwaysReturnListAndOK() {
+		when()
+			.get()
+		.then()
+			.statusCode(Response.Status.OK.getStatusCode())
+			.body(matchesPattern("^\\s*\\[.*\\]\\s*$"));
+	}
+
+	@Test
+	void findWithFloatParameterShouldReturnNotFound() {
+		given()
+			.pathParam("id", 1.23)
+		.when()
+			.get("{id}")
+		.then()
+			.statusCode(Response.Status.NOT_FOUND.getStatusCode());
+	}
+
+	@Test
+	void findWithValidParameterShouldReturnOKOrNotFound() {
+		given()
+			.pathParam("id", new Random().nextLong())
+		.when()
+			.get("{id}")
+		.then()
+			.statusCode(is(in(Arrays.asList(
+				Response.Status.OK.getStatusCode(),
+				Response.Status.NOT_FOUND.getStatusCode())
+			)));
+	}
+}
---
+++ src/test/java/com/endpoints/ReviewResourceTest.java
@@ -0,0 +1,23 @@
+package com.endpoints;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.matchesPattern;
+
+@QuarkusTest
+@TestHTTPEndpoint(ReviewResource.class)
+public class ReviewResourceTest {
+
+	@Test
+	void findAllShouldAlwaysReturnListAndOK() {
+		when()
+			.get()
+		.then()
+			.statusCode(Response.Status.OK.getStatusCode())
+			.body(matchesPattern("^\\s*\\[.*\\]\\s*$"));
+	}
+}
